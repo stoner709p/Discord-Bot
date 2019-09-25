bcc7cade288d74f3a00c3a5bdf76f2d90fd508b8
stoner709p <44442888+stoner709p@users.noreply.github.com>
2019-09-25 16:25:30 -0500

    Update Hack.js

--- Cmds/Hack.js
+++ Cmds/Hack.js
@@ -2,8 +2,10 @@ const fs = require("fs")
 
 
 module.exports.run = async (bot, message, args) => {
+	
 	const r = Math.random().toString(36).substring(7);
     const user = message.mentions.users.first();
+	if(!user){return(message.channel.send("You need a person to hack!")}
     if (user) {
       const member = message.guild.member(user);
       if (member) {
